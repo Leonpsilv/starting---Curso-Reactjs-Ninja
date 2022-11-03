718adec632acc77a30aee27e27dd1b0e4231492b
Leonpsilv <leonardo.pin.s2002@gmail.com>
2022-11-03 18:11:36 -0400

    JSX and react hot loader

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 'use strict'
-import Title from './app';
+import App from './app';
 import React from 'react';
 import { render } from 'react-dom';
+import { AppContainer } from 'react-hot-loader';
 
-render(
-    React.createElement(Title),
-    document.querySelector('[data-js="app"')
-)
+const renderApp = (NextApp) => {
+    render(
+        <AppContainer>
+            <NextApp/>
+        </AppContainer>,
+        document.querySelector('[data-js="app"')
+    )
+}
+
+renderApp(App);
+
+if (module.hot) {
+    module.hot.accept('./app', () => {
+        const NextApp = require('./app').default
+        renderApp(NextApp);
+    })
+}
